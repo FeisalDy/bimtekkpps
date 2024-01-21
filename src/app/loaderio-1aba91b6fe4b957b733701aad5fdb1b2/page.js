0142bc0c83c7e35e94cb3ce35ca0ce8e0d87a6a9
feisaldy <feisaldy26@gmail.com>
2024-01-21 18:52:52 +0700

    add aumotic download token file

--- src/app/loaderio-1aba91b6fe4b957b733701aad5fdb1b2/page.js
+++ src/app/loaderio-1aba91b6fe4b957b733701aad5fdb1b2/page.js
@@ -1,7 +1,25 @@
-import React from 'react'
+'use client'
+import React, { useEffect } from 'react'
 
 const Test = () => {
-  return <div>loaderio-1aba91b6fe4b957b733701aad5fdb1b2</div>
+  useEffect(() => {
+    // Simulate a click on the anchor link to trigger the download
+    const anchorLink = document.getElementById('downloadLink')
+    if (anchorLink) {
+      anchorLink.click()
+    }
+  }, [])
+
+  return (
+    <a
+      id='downloadLink'
+      style={{ display: 'none' }}
+      download
+      href='loaderio-1aba91b6fe4b957b733701aad5fdb1b2.txt'
+    >
+      Loading...
+    </a>
+  )
 }
 
 export default Test
